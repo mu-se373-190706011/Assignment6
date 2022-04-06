fc9c97977a2d24debb35b02baa3873ef72fb92d6
mu-se373-190706011 <101144051+mu-se373-190706011@users.noreply.github.com>
2022-04-06 11:41:29 +0300

    Create db_config.php

---
+++ db_config.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+define('DB_username', 'root'); 
+define('DB_password', ''); 
+define('DB_name', 'root'); 
+define('DB_surname', 'root'); 
+define('DB_DATABASE', 'city_tour'); 
+define('DB_SERVER', 'localhost:8080'); 
